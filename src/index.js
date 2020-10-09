3a67c451510db1fb029c446df071526e2d1aece1
Ulyana Zhivotenkova <ulyanazh2@gmail.com>
2020-10-09 21:59:25 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let objBrackets  = Object.fromEntries(bracketsConfig);
+    let arr = [];    
+      for (let i of str.split('')) {
+          if (arr[0] === i) {
+             arr.shift();
+          } else if (objBrackets[i]) {
+            arr.unshift(objBrackets[i]);
+          } else {
+              return false;
+          }
+      }    
+      return arr.length === 0;
 }
